e90230bad9e1a406f7b33c17eb10feb1ed9d9973
Daniel Salas <danielcraft236@hotmail.com>
2020-03-04 20:39:36 -0500

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,7 +48,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTreeModule} from '@angular/material/tree';
-
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -107,6 +107,7 @@ import {MatTreeModule} from '@angular/material/tree';
     MatTreeModule,
     PortalModule,
     ScrollingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-
+  nombre = new FormControl('');
+  clave = new FormControl('');
   constructor() { }
 
   ngOnInit(): void {
   }
-
+onIniciar(){
+  if(this.nombre.value == 'admin' && this.clave.value == '123'){
+   console.log('Inicio correcto');
+  }else{
+   console.log('Datos incorrectos');
+  }
+}
 }
